4add91958cea6a3bb752693a6ea0fa278f95912c
Alexis Sierra <asierra@bit.lat>
2023-12-22 09:03:30 -0600

    paleta de colores

---
+++ tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-1': {
+          DEFAULT: '#A925C3',
+        },
+        'primary-2': {
+          DEFAULT: '#532DEE',
+        },
+        'secondary-1': {
+          DEFAULT: '#1406E0',
+        },
+        'secondary-2': {
+          DEFAULT: '#7A03F9',
+        },
+        ebony: {
+          DEFAULT: '#0C0919',
+        },
+        haiti: {
+          DEFAULT: '#191139',
+        },
+        'jordy-blue': {
+          DEFAULT: '#9CA1FA',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
